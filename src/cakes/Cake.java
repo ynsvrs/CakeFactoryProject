84ec19c30680d984c82c03666cbb8bd4ac51ade2
ysnvrs <saniyadair@gmail.com>
2025-09-22 17:38:59 +0500

    Updated version

--- src/cakes/Cake.java
+++ src/cakes/Cake.java
@@ -2,7 +2,7 @@ package cakes;
 
 public abstract class Cake {
     public void bake() {
-        System.out.println("Baking cake for 30 minutes at at 180 degrees...");
+        System.out.println("Baking cake for 30 minutes at 180 degrees...");
     }
     public abstract void eat();
 }
